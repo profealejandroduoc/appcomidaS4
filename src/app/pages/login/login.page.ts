fa2187d45f712902890b116722a380ddccd5c73c
El profe <al.sepulvedam@profesor.duoc.clean>
2024-10-21 14:34:44 -0300

    login ok

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { Usuario } from 'src/app/interfaces/iusuario';
+import { ServiciodbService } from 'src/app/services/serviciodb.service';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  usr:Usuario={
+    username:'',
+    password:'',
+    nombre:'',
+    apellido:''
+    }
+
+  constructor(private router:Router,
+    private toastController:ToastController,
+    private db:ServiciodbService) { }
 
   ngOnInit() {
   }
 
   onSubmit(){
+    let buscado=this.db.leer(this.usr.username);
+    buscado.then(usuarioBuscado=>{
+      if(usuarioBuscado!==null){
+        this.router.navigate(['/home']);
+      }
+      else{
+       this.presentToast('top');
+      }
+    })
+
     
+  }
+
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      header:'ERROR',
+      message: 'Usuario y/o Contraseña incorrecta!!!',
+      duration: 2500,
+      position: position,
+    });
+
+    await toast.present();
   }
 }
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 import { Usuario } from 'src/app/interfaces/iusuario';
 import { ServiciodbService } from 'src/app/services/serviciodb.service';
 
@@ -9,8 +10,6 @@ import { ServiciodbService } from 'src/app/services/serviciodb.service';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-
-
   usr:Usuario={
     username:'',
     password:'',
@@ -19,12 +18,54 @@ export class RegistroPage implements OnInit {
     }
 
   constructor(private db:ServiciodbService,
-    private router:Router) { }
+    private router:Router,
+    private alertController:AlertController,
+    private toastController:ToastController
+    ) { }
 
   ngOnInit() {
   }
 
   registrar(){
+    let buscado=this.db.leer(this.usr.username);
+    buscado.then(usuarioBuscado=>{
+      if(usuarioBuscado===null){
+        this.db.guardar(this.usr.username,this.usr);
+        this.presentAlert();
+      }
+      else{
+        console.log("EL usuario ya existe");
+        this.presentToast('top');
+      }
+    })
+
     
+  }
+
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Usuario Registrado ',
+      buttons: [{
+        text:'Aceptar',
+        handler:()=>{
+          this.router.navigate(['/login']);
+        }
+      }],
+    });
+
+    await alert.present();
+  }
+
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      header:'ERROR',
+      message: 'Usuario ya existe!!!',
+      duration: 2500,
+      position: position,
+    });
+
+    await toast.present();
   }
 }
